399d86d64ae758ca6b1a7c0fb4394bf05d1190f3
javachen <june.chan@foxmail.com>
2015-06-23 15:05:15 +0800

    update:update

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo `date`
+
+msg="update:$*"
+
+
+qrsync qiniu-images.conf
+
+cd _posts
+
+sed -i "" 's/\/static\/images/http:\/\/javachen\-rs\.qiniudn\.com\/images/g' */*
+
+cd ..
+
+rm -rf _site/*
+jekyll build
+
+git add --all ./*
+git status
+git commit -m "$msg"
+git push origin master
+
+cd ../javachen.github.io
+
+rm -rf {20*,page*,*.html,*.xml,*.txt,*.sh}
+cp -r ../javachen-blog/_site/* .
+
+git add --all ./*
+git commit -m "$msg"
+git push origin gitcafe-pages
+
+echo `date`
+exit 0
