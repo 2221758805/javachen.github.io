f2998e7ec1711709e80eaf6b834bd18d876f6696
yuke.czj <yuke.czj@alibaba-inc.com>
2016-02-25 10:23:19 +0800

    update at 2016年 2月25日 星期四 10时23分19秒 CST update:

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo `date`
+
+msg="update:$*"
+
+
+qrsync qiniu-images.json
+
+
+cd _posts
+
+sed -i "" 's/javachen\-rs\.qiniudn\.com/7xnrdo\.com1\.z0\.glb.clouddn\.com/g' */*
+
+cd ..
+
+rm -rf _site/*
+jekyll build
+
+git add --all ./*
+git status
+git commit -m "update at `date` $msg"
+git push origin master
+
+echo "post sitemap.txt to baidu"
+curl -H 'Content-Type:text/plain' --data-binary @_site/sitemap.txt "http://data.zz.baidu.com/urls?site=blog.javachen.com&token=2CeQfTIrbOgmAqpv"
+
+cd ../javachen.github.io
+
+rm -rf {20*,page*,*.html,*.xml,*.txt,*.sh}
+cp -r ../javachen-blog/_site/* .
+
+git add --all ./*
+git commit -m "update at `date` $msg"
+git push origin master
+
+cd ../javachen.gitcafe.io
+
+rm -rf {20*,page*,*.html,*.xml,*.txt,*.sh}
+cp -r ../javachen-blog/_site/* .
+
+git add --all ./*
+git commit -m "update at `date` $msg"
+git push origin gitcafe-pages
+
+echo `date`
+exit 0
