1c75ec7026d9fdec8e24fbdae2a1879458891351
yuke <junezchen@gmail.com>
2016-04-01 14:51:37 +0800

    update at 2016年 4月 1日 星期五 14时51分37秒 CST update:up

--- deploy.sh
+++ deploy.sh
@@ -34,14 +34,5 @@ git add --all ./*
 git commit -m "update at `date` $msg"
 git push origin master
 
-cd ../javachen.gitcafe.io
-git pull
-rm -rf {20*,page*,*.html,*.xml,*.txt,*.sh}
-cp -r ../javachen-blog/_site/* .
-
-git add --all ./*
-git commit -m "update at `date` $msg"
-git push origin gitcafe-pages
-
 echo `date`
 exit 0
